f02c52da3ec996c6eec968cefc3b76d76105e809
Emerson Schulze <emerson7e@gmail.com>
2017-08-22 22:39:15 -0300

    Antes de resolver o conflito foi commitado

--- SerenityCucumber/src/test/java/Business/Calculadora.java
+++ SerenityCucumber/src/test/java/Business/Calculadora.java
@@ -2,6 +2,7 @@ package Business;
 
 	public interface Calculadora {
 	    
-	    public int calcular (Operador operador, int valor1, int valor2);
-	    
+	    public int calcular (Operador operadorr, int valor1, int valor2);
+
+
 	}
--- SerenityCucumber/src/test/java/Business/CalculadoraCore.java
+++ SerenityCucumber/src/test/java/Business/CalculadoraCore.java
@@ -3,19 +3,19 @@ package Business;
 public class CalculadoraCore {
 
 	public int somar(Integer valorA, Integer valorB){
-		return valorA + valorB;
+	return valorA + valorB;
 	}
 	
 	public int subtrair(Integer valorA, Integer valorB){
-		return valorA - valorB;
+	return valorA - valorB;
 	}
 	
 	public int dividir(Integer valorA, Integer valorB){
-		return valorA / valorB;
+	return valorA / valorB;
 	}
 	
 	public int multiplicar(Integer valorA, Integer valorB){
-		return valorA * valorB;
+	return valorA * valorB;
 	}
 	
 }
